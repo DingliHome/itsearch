007af39edc19e92c50d6545e8fa858c5a324e536
sagetouch <sagetouch@sage>
2014-06-26 14:22:51 +0800

    add

--- src/com/MyApplication.java
+++ src/com/MyApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import com.baidu.mapapi.SDKInitializer;
 
+import Helper.LogHelper;
 import android.app.Application;
 
 public class MyApplication extends Application {
@@ -9,6 +10,7 @@ public class MyApplication extends Application {
 	@Override
 	public void onCreate() {
 		SDKInitializer.initialize(this);
+		LogHelper.getInstance(this).start();
 		super.onCreate();
 	}
 }
